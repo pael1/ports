8b43892f2548e1b07641ad00d4ae4a1543c54741
pael1 <rafaelfernandez677@gmail.com>
2022-04-07 12:32:49 +0800

    arranged code

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-
         Yajra\DataTables\DataTablesServiceProvider::class,
 
     ],
@@ -232,7 +231,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
